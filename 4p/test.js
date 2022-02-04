5f855f011f80d1348a3668f3b1fc878ed9881ef8
Pavel Burgr <pb@drake.cz>
2022-02-04 11:55:55 +0100

    WIP

--- 4p/test.js
+++ 4p/test.js
@@ -1,35 +1,67 @@
 
 
 let calib = [
-    /* 0 */ { x: -1300, y: +500, a: 1000, b: 2000 },
-    /* 1 */ { x: +1200, y: +450, a: 2000, b: 1000 },
-    /* 2 */ { x: +1250, y: -550, a: 3000, b: 1500 },
-    /* 3 */ { x: -1100, y: -520, a: 1500, b: 3000 }
+    /* 0 */ { x: -1173, y: +519, a: 970, b: 2987 },
+    /* 1 */ { x: +1164, y: +497, a: 2985, b: 993 },
+    /* 2 */ { x: +1072, y: -524, a: 3337, b: 1961 },
+    /* 3 */ { x: -1151, y: -561, a: 1972, b: 3424 },
+
+    // /* 0 */ { x: -248, y: +136, a: 1883, b: 2288 },
+    // /* 1 */ { x: +124, y: +116, a: 2194, b: 1992 },
+    // /* 2 */ { x: +144, y: -202, a: 2400, b: 2186 },
+    // /* 3 */ { x: -174, y: -210, a: 2171, b: 2428 },
+
 ];
 
-let x = -1100;
-let y = -520;
+let x = -1162;
+let y = 77;
 
-let cnt = 0;
-let sumA = 0;
-let sumB = 0;
 
-for (let c0 = 0; c0 < calib.length; c0++) {
-    for (let c1 = 0; c1 < calib.length; c1++) {
-        if (c0 !== c1) {
+let data = [
+    { x: 0, y: 100 },
+    { x: 100, y: 200 },
+    { x: 200, y: 150 }
+];
 
-            let p = Math.hypot(x - calib[c0].x, y - calib[c0].y) / Math.hypot(calib[c1].x - calib[c0].x, calib[c1].y - calib[c0].y);
-            let a = calib[c0].a + (calib[c1].a - calib[c0].a) * p;
-            let b = calib[c0].b + (calib[c1].b - calib[c0].b) * p;
-            console.info(`${c0} x ${c1} ${a.toFixed(0)} ${b.toFixed(0)}`);
-            sumA += a;
-            sumB += b;
-            cnt++;
+function polynom(multipliers, x) {
+    return multipliers.map((m, i) => multipliers[i] * Math.pow(x, i)).reduce((acc, y) => acc + y, 0);
+}
+
+let powers = [0, 1, 2];
+let maxS = [500, 5, 0.05];
+let solution;
+
+function sweep(multipliers) {
+
+    let ms = maxS[multipliers.length];
+    for (let s = -ms; s <= ms; s = s + ms / 100) {
+        let m = [...multipliers, s];
+        if (m.length < powers.length) {
+            sweep(m);
+        } else {
+            let errors = [];
+            for (let i in data) {
+                let result = polynom(m, data[i].x);
+                errors[i] = data[i].y - result;
+            }
+            let error = errors.map(e => e * e).reduce((acc, e) => acc + e, 0);
+            //console.info(m.map(n => n.toFixed(4)), errors, error);
+            if (!solution || solution.error > error) {
+                solution = {error, multipliers: m};
+            }
         }
     }
 }
 
-let a = sumA / cnt;
-let b = sumB / cnt;
-console.info(`${a.toFixed(0)} ${b.toFixed(0)}`);
+let t0 = new Date().getTime();
+sweep([]);
+let t1 = new Date().getTime();
 
+console.info("Min:", solution);
+console.info("Check:");
+for (let i in data) {
+    let result = polynom(solution.multipliers, data[i].x);
+    console.info(`${i}: [${data[i].x},${data[i].y}]: ${result} (${((data[i].y - result) / data[i].y * 100).toFixed(2)}% error)`);
+}
+console.info(`${(t1 - t0) / 1000} sec.`);
+console.info("plot "+solution.multipliers.map((m,i) => `${m}*x^${i}`).join("+"));
