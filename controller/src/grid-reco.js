439ef12219f534a246c29b3c4a42bcfa10845f7a
Pavel Burgr <pb@drake.cz>
2022-03-06 21:48:36 +0100

    WIP

--- controller/src/grid-reco.js
+++ controller/src/grid-reco.js
@@ -1,52 +1,47 @@
 module.exports = () => {
 
-    function calculateHistogram(image, size) {
-        let histograms = [];
+    function index(a, b, size, dir) {
+        return dir === 0 ? a + b * size : b + a * size;
+    }
 
-        // allocate histograms
-        for (let dir = 0; dir <= 1; dir++) {
-            histograms[dir] = new Uint32Array(new ArrayBuffer(size * 4));
-        }
+    function calculateHistogram(image, size, dir) {
+
+        let histogram = new Uint32Array(new ArrayBuffer(size * 4));
 
         // count histograms sums
-        for (let x = 0; x < size; x++) {
-            for (let y = 0; y < size; y++) {
-                v = image[x + size * y];
-                histograms[0][x] += v;
-                histograms[1][y] += v;
+        for (let a = 0; a < size; a++) {
+            for (let b = 0; b < size; b++) {
+                v = image[index(a, b, size, dir)];
+                histogram[a] += v;
             }
         }
 
         // normalize to 0..100
-        for (let dir = 0; dir <= 1; dir++) {
-
-            let min;
-            let max;
-            for (let i = 0; i < size; i++) {
-                let v = histograms[dir][i];
-                if (min === undefined || min > v) {
-                    min = v;
-                }
-                if (max === undefined || max < v) {
-                    max = v;
-                }
+        let min;
+        let max;
+        for (let i = 0; i < size; i++) {
+            let v = histogram[i];
+            if (min === undefined || min > v) {
+                min = v;
             }
-
-            let sum = 0;
-            for (let i = 0; i < size; i++) {
-                histograms[dir][i] = max === min ? 0 : 100 * (histograms[dir][i] - min) / (max - min);
-                sum += histograms[dir][i];
+            if (max === undefined || max < v) {
+                max = v;
             }
-            histograms[dir].avg = sum / size;
-
-            // calculate histogram of histogram
-            let hoh = new Uint32Array(new ArrayBuffer(101 * 4));
-            for (let i = 0; i <= 100; i++) {
-                hoh[histograms[dir][i]]++;
-            }
-            // identify empty image with noise only
-            histograms[dir].clean = hoh.some(v => v > 15) && hoh[0] !== 101;
         }
+        let sum = 0;
+        for (let i = 0; i < size; i++) {
+            histogram[i] = max === min ? 0 : 100 * (histogram[i] - min) / (max - min);
+            sum += histogram[i];
+        }
+        histogram.avg = sum / size;
+
+        // calculate histogram of histogram
+        let hoh = new Uint32Array(new ArrayBuffer(101 * 4));
+        for (let i = 0; i <= 100; i++) {
+            hoh[histogram[i]]++;
+        }
+        // identify empty image with noise only
+        histogram.clean = hoh.some(v => v > 15) && hoh[0] !== 101;
 
         // identify flat histogram, find centers of maximum band if any 
         for (let dir = 0; dir <= 1; dir++) {
@@ -54,11 +49,11 @@ module.exports = () => {
             let start;
             let stop;
 
-            if (histograms[dir].clean && histograms[dir].avg < 70) {
+            if (histogram.clean && histogram.avg < 70) {
 
                 for (let i = 0; i < size; i++) {
-                    let v = histograms[dir][i];
-                    if (v > (histograms[dir].avg + 100) / 2) {
+                    let v = histogram[i];
+                    if (v > (histogram.avg + 100) / 2) {
                         if (start === undefined) {
                             start = i;
                         }
@@ -80,17 +75,17 @@ module.exports = () => {
 
                 for (let i = 0; i < start - size / 10; i++) {
                     loSide.count++;
-                    loSide.sum += histograms[dir][i];
+                    loSide.sum += histogram[i];
                 }
 
                 for (let i = stop + size / 10; i < size; i++) {
                     hiSide.count++;
-                    hiSide.sum += histograms[dir][i];
+                    hiSide.sum += histogram[i];
                 }
 
-                histograms[dir].peak = (start + stop) / 2;
+                histogram.peak = (start + stop) / 2;
 
-                histograms[dir].sides = Math.sign(Math.round((
+                histogram.sides = Math.sign(Math.round((
                     (loSide.count && loSide.sum / loSide.count) -
                     (hiSide.count && hiSide.sum / hiSide.count)
                 ) / 3) * 3) + 1;
@@ -98,14 +93,17 @@ module.exports = () => {
             }
         }
 
-        return histograms;
+        return histogram;
     }
 
     function recognize(image, size, debug) {
 
         let t0 = new Date().getTime();
 
-        let histograms = calculateHistogram(image, size, debug);
+        let histograms = [
+            calculateHistogram(image, size, 0),
+            calculateHistogram(image, size, 1)
+        ];
 
         let shapes = {
             0: ["northwest", "north", "northeast"],
