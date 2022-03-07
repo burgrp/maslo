09355180e0ef6cc52126c43531959ce36cdc41a4
Pavel Burgr <pb@drake.cz>
2022-03-08 00:30:43 +0100

    WIP

--- controller/src/machine.js
+++ controller/src/machine.js
@@ -1,13 +1,6 @@
 const logError = require("debug")("app:machine:error");
 const logInfo = require("debug")("app:machine:info");
 
-function distanceMmToAbsSteps(motorConfig, distanceMm) {
-    return distanceMm * motorConfig.stepsPerMm;
-}
-
-function absStepsToDistanceMm(motorConfig, steps) {
-    return steps / motorConfig.stepsPerMm;
-}
 
 let pow2 = a => a * a;
 let { sqrt, pow, hypot, abs, round, min, max, sign } = Math;
@@ -18,7 +11,8 @@ const MODE_JOB = "JOB";
 module.exports = async ({
     drivers,
     driver,
-    config
+    config,
+    checks
 }) => {
 
     let model = {
@@ -58,27 +52,6 @@ module.exports = async ({
 
     let waiters = [];
 
-    function userToMachineCS(pos) {
-        return {
-            xMm: pos.xMm,
-            yMm: config.beam.motorsToWorkspaceMm + config.workspace.heightMm / 2 - pos.yMm
-        }
-    }
-
-    function machineToUserCS(pos) {
-        return {
-            xMm: pos.xMm,
-            yMm: config.beam.motorsToWorkspaceMm + config.workspace.heightMm / 2 - pos.yMm
-        }
-    }
-
-    function getChainLengths(positionUCS) {
-        let positionMCS = userToMachineCS(positionUCS);
-        return {
-            aMm: hypot(config.beam.motorsDistanceMm / 2 + positionMCS.xMm, positionMCS.yMm),
-            bMm: hypot(config.beam.motorsDistanceMm / 2 - positionMCS.xMm, positionMCS.yMm)
-        };
-    }
 
     async function checkModel() {
 
@@ -106,148 +79,6 @@ module.exports = async ({
                     logError(`Relay ${name} error on get:`, e);
                     delete r.state;
                     model.errors[`relay.${name}.get`] = e.message || e;
-                }
-            }
-        }
-
-        function checkSledPosition() {
-            if (model.motors.a.state && model.motors.b.state) {
-
-                if (
-                    !Number.isFinite(model.sled.xMm) &&
-                    !Number.isFinite(model.sled.yMm) &&
-                    Number.isFinite(config.lastPosition.xMm) &&
-                    Number.isFinite(config.lastPosition.yMm)
-                ) {
-                    model.sled.reference = {
-                        xMm: config.lastPosition.xMm,
-                        yMm: config.lastPosition.yMm,
-                        aSteps: model.motors.a.state.steps,
-                        bSteps: model.motors.b.state.steps
-                    };
-                }
-
-                if (model.sled.reference) {
-
-                    let referenceMCS = userToMachineCS(model.sled.reference);
-
-                    let referenceASteps = distanceMmToAbsSteps(
-                        config.motors.a,
-                        hypot(
-                            config.beam.motorsDistanceMm / 2 + referenceMCS.xMm,
-                            referenceMCS.yMm
-                        )
-                    ) - model.sled.reference.aSteps;
-
-                    let referenceBSteps = distanceMmToAbsSteps(
-                        config.motors.b,
-                        hypot(
-                            config.beam.motorsDistanceMm / 2 - referenceMCS.xMm,
-                            referenceMCS.yMm
-                        )
-                    ) - model.sled.reference.bSteps;
-
-                    // let's have triangle MotorA-MotorB-Sled, then:
-                    // a is MotorA-Sled, i.e. chain length a
-                    // b is MotorA-Sled, i.e. chain length b
-                    // aa is identical to MotorA-MotorB, going from MotorA to intersection with vertical from Sled
-                    let a = absStepsToDistanceMm(config.motors.a, referenceASteps + model.motors.a.state.steps);
-                    let b = absStepsToDistanceMm(config.motors.b, referenceBSteps + model.motors.b.state.steps);
-                    let aa = (pow2(a) - pow2(b) + pow2(config.beam.motorsDistanceMm)) / (2 * config.beam.motorsDistanceMm);
-
-                    let position = machineToUserCS({
-                        xMm: aa - config.beam.motorsDistanceMm / 2,
-                        yMm: sqrt(pow2(a) - pow2(aa))
-                    });
-
-                    model.sled.xMm = position.xMm;
-                    model.sled.yMm = position.yMm;
-
-                } else {
-                    delete model.sled.xMm;
-                    delete model.sled.yMm;
-                }
-
-            } else {
-                delete model.sled.xMm;
-                delete model.sled.yMm;
-            }
-            config.lastPosition.xMm = Math.round(model.sled.xMm * 1000) / 1000;
-            config.lastPosition.yMm = Math.round(model.sled.yMm * 1000) / 1000;
-            if (!Number.isFinite(config.lastPosition.xMm) || !Number.isFinite(config.lastPosition.yMm)) {
-                delete config.lastPosition.xMm;
-                delete config.lastPosition.yMm;
-            }
-        }
-
-        function checkSpindlePosition() {
-            if (model.motors.z.state) {
-
-                if (!Number.isFinite(model.spindle.zMm) &&
-                    Number.isFinite(config.lastPosition.zMm)) {
-                    model.spindle.reference = {
-                        zMm: config.lastPosition.zMm,
-                        zSteps: model.motors.z.state.steps
-                    };
-                }
-
-                if (model.spindle.reference) {
-                    model.spindle.zMm = model.spindle.reference.zMm + absStepsToDistanceMm(config.motors.z, model.motors.z.state.steps - model.spindle.reference.zSteps);
-                } else {
-                    delete model.spindle.zMm;
-                }
-
-            } else {
-                delete model.spindle.zMm;
-            }
-            config.lastPosition.zMm = Math.round(model.spindle.zMm * 1000) / 1000;
-            if (!Number.isFinite(config.lastPosition.zMm)) {
-                delete config.lastPosition.zMm;
-            }
-        }
-
-        async function checkTarget() {
-
-            if (
-                Number.isFinite(model.sled.xMm) &&
-                Number.isFinite(model.sled.yMm) &&
-                Number.isFinite(model.spindle.zMm) &&
-                model.target
-            ) {
-
-                let targetChains = getChainLengths(model.target);
-                let sledChains = getChainLengths(model.sled);
-
-                for (let m of ["a", "b", "z"]) {
-
-                    let offset = m === "z" ?
-                        model.spindle.zMm - model.target.zMm :
-                        targetChains[m + "Mm"] - sledChains[m + "Mm"];
-
-                    let motor = model.motors[m];
-
-                    let duty = 0;
-
-                    if (abs(offset) > 0.3) {
-
-                        duty = (offset - (motor.offset || 0) / 2) * config.motors[m].offsetToDuty;
-
-                        duty = sign(duty) * min(abs(duty), 1);
-                        duty = sign(duty) * pow(abs(duty), 1 / 4);
-
-                        //duty = (motor.duty + duty) / 2;
-
-                        if (abs(duty - motor.duty) > 0.4 && sign(duty) === -sign(motor.duty)) {
-                            duty = 0;
-                        }
-                    }
-
-                    motor.duty = duty || 0;
-                    motor.offset = offset;
-                }
-            } else {
-                for (let m of ["a", "b", "z"]) {
-                    delete model.motors[m].offset;
                 }
             }
         }
@@ -298,13 +129,14 @@ module.exports = async ({
 
         await checkMotorStates();
         await checkRelayStates();
-        await checkSledPosition();
-        await checkSpindlePosition();
-        await checkTarget();
         await checkHooverRelay();
         await checkJobSynchronizers();
         await setMotorDuties();
         await setRelayStates();
+
+        for (let check of checks) {
+            await check(model);
+        }
     }
 
     async function machineCheckLoop() {
